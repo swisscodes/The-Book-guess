1177ea7795a5734ed334a08258f1eb28d6baa767
Obafemi Lawal <swissbobo@gmail.com>
2022-08-08 14:32:20 +0300

    completed the rust guessing game tutorial

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            //Result object was return into this scope
+            //Result::Ok is available so does Result::Err
+            //But we could use them directly like blow also.
+            Ok(num) => num,
+            Err(_) => {
+                println!("please enter number only !! \n");
+                continue
+            }
+        };
+
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
